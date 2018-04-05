82bbe4a2ac6836b8dbdf37178b94940ea7f84bdd
bubbathevtog <bubbathevtog@gmail.com>
2018-04-05 18:19:28 -0400

    Added first page, also added template file.
    I have added an introduction page that we can mess with. I've also added
    a template file for easy copy pasta.
    
    + /pages/TheCommandLine/introduction.php
    + /pages/.template.php

---
+++ pages/.template.php
@@ -0,0 +1,16 @@
+<?php
+	$title='PATLS : ';
+	$path='';
+	$nav='';
+	include($path.'assets/inc/header.php');
+?>
+<body>
+	<div class="content">
+		<h1></h1>
+		<p></p>
+	</div>
+</body>
+<?php
+	include($path.'assets/inc/footer.php');
+?>
+
---
+++ pages/TheCommandLine/introduction.php
@@ -0,0 +1,21 @@
+<?php
+	$title='PATLS : The Command Line';
+	$path='../../';
+	$nav='introduction';
+	include($path.'assets/inc/header.php');
+?>
+<body>
+	<div class="content">
+		<h1>Introduction</h1>
+		<p>
+			The command line is where it all happens in Linux.
+			 Every Linux distribution contains the command line, and most likely everyone who uses Linux will use the command line.
+			 You might be wondering how you’re going to navigate through a computer using just text!
+			 I mean, isn’t that what the mouse and graphics were invented for?
+			 In this module, you are going to learn about how to efficiently navigate the UNIX command line, and do common tasks you would do with a mouse using just your keyboard and a terminal window.
+		 </p>
+	</div>
+</body>
+<?php
+	include($path.'assets/inc/footer.php');
+?>
