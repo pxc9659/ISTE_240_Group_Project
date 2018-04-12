b133aafa71c022c2975115e3d4f1a8ed2ea3a5c2
bubbathevtog <bubbathevtog@gmail.com>
2018-04-12 19:50:41 -0400

    LARGE COMMIT
    I have added all of the images to all of the pages.
    
    I've moved the </head>, <body>, and <main> tags back to the head. I then
    formatted each page accordingly.
    
    I've also commented more of the CSS and added a LARGE comments to each
    section.

--- pages/TheCommandLine/pwd.php
+++ pages/TheCommandLine/pwd.php
@@ -4,15 +4,18 @@
 	$nav='pwd';
 	include($path.'assets/inc/head.php');
 ?>
-</head>
-</body>
-	<main>
-		<h2>pwd</h2>
-		<p>
-			This command is used to figure out what folder you are currently located in.
-			 Typing ‘pwd’ into a command line will give you the path to this folder.
-			 This is useful when you are trying to navigate through directories, and for knowing that you are in the right directory before executing a particular command.
-		</p>
+<h2>palts@tcl$ pwd</h2>
+<hr>
+<picture class="titlePicture">
+	<source media="(max-width: 450px)" srcset="https://i.imgur.com/aybQkA5.png">
+	<source media="(max-width: 1024px)" srcset="https://i.imgur.com/mYooWNA.png">
+	<img src="https://i.imgur.com/CEEor77.png" alt="An example of the pwd command being ran.">
+</picture>
+<p>
+	This command is used to figure out what folder you are currently located in.
+	 Typing ‘pwd’ into a command line will give you the path to this folder.
+	 This is useful when you are trying to navigate through directories, and for knowing that you are in the right directory before executing a particular command.
+</p>
 <?php
 	include($path.'assets/inc/footer.php');
 ?>
