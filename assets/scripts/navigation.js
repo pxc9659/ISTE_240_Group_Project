912786cbfae80bc82b92df8ef2b0c877d6ac38c1
bubbathevtog <bubbathevtog@gmail.com>
2018-04-15 03:16:06 -0400

    This is a navigable site.

--- assets/scripts/navigation.js
+++ assets/scripts/navigation.js
@@ -1,14 +1,23 @@
-var mainMarginLeft;
-var xSize="100%";
-
-function closeNavMobile(){
-	document.getElementById("navDiv").style.width="0";
-	document.body.style.marginLeft=mainMarginLeft;
-	document.body.style.marginLeft="0%";
-}
+var defaultMarginLeft="10%";
 
 function openNavMobile(){
-	mainMarginLeft = document.body.style.marginLeft;
-	document.getElementById("navDiv").style.width=xSize;
-	document.body.style.marginLeft=xSize;
+	let width = window.innerWidth;
+	if(width <= 450){
+		document.getElementById("navDiv").style.width="100%";
+		document.body.style.marginLeft="0";
+	}else if(width >450 && width < 1025){
+		document.getElementById("navDiv").style.width="250px";
+		document.body.style.marginLeft="250px";
+	}
+}
+
+function closeNavMobile(){
+	let width = window.innerWidth;
+	if(width <= 450){
+		document.getElementById("navDiv").style.width="0%";
+		document.body.style.marginLeft="0%";
+	}else if(width > 450 && width < 1025){
+		document.getElementById("navDiv").style.width="0%";
+		document.body.style.marginLeft=defaultMarginLeft;
+	}
 }
