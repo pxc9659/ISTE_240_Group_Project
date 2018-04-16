d11b9c0217565e39cc532ef20f89e907f15986e4
justein230 <justein230@gmail.com>
2018-04-16 19:51:10 -0400

    Removed code that was covered by head.php

--- index.php
+++ index.php
@@ -5,23 +5,18 @@
 	include($path.'assets/inc/head.php');
 	// include($path.'assets/inc/nav.php');
 ?>
-</head>
-<body>
-	<main>
-		<h1>Home Page</h1>
+	<h1>Home Page</h1>
 		
-		<h2>Our goal</h2>
+	<h2>Our goal</h2>
 		
-		<p>POP & LOCK the STACK is a website with references and tutorials on UNIX development for beginners and novice users. 
-		The main goal of this site is to be educative and be a helpful tool for developers to use it as a guide for their work. 
-		This website gives the user an idea of all the basic commands a developer should know to work in an UNIX environment. 
-		It teaches them all the basic commands and flags associated with each command.</p>
+	<p>POP & LOCK the STACK is a website with references and tutorials on UNIX development for beginners and novice users. 
+	The main goal of this site is to be educative and be a helpful tool for developers to use it as a guide for their work. 
+	This website gives the user an idea of all the basic commands a developer should know to work in an UNIX environment. 
+	It teaches them all the basic commands and flags associated with each command.</p>
 		
-		<h2>A little about us</h2>
-		<p>This site is a free resource for developers and always will be. 
-		POP & LOCK the STACK was created by a group of 5 members as a group project for their web development class.</p>
-	</main>
-</body>
+	<h2>A little about us</h2>
+	<p>This site is a free resource for developers and always will be. 
+	POP & LOCK the STACK was created by a group of 5 members as a group project for their web development class.</p>
 <?php
 	include($path.'assets/inc/footer.php');
 ?>
