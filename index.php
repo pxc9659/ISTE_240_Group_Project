6abce20b62f41637e64a29d0f8b8bd474e8e2a92
Vignesh111 <vk4534@g.rit.edu>
2018-04-16 18:50:54 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -8,7 +8,18 @@
 </head>
 <body>
 	<main>
-		<h1>We'll make something of this I'm sure...</h1>
+		<h1>Home Page</h1>
+		
+		<h2>Our goal</h2>
+		
+		<p>POP & LOCK the STACK is a website with references and tutorials on UNIX development for beginners and novice users. 
+		The main goal of this site is to be educative and be a helpful tool for developers to use it as a guide for their work. 
+		This website gives the user an idea of all the basic commands a developer should know to work in an UNIX environment. 
+		It teaches them all the basic commands and flags associated with each command.</p>
+		
+		<h2>A little about us</h2>
+		<p>This site is a free resource for developers and always will be. 
+		POP & LOCK the STACK was created by a group of 5 members as a group project for their web development class.</p>
 	</main>
 </body>
 <?php
