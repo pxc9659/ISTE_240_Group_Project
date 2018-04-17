fe30f423c97b2345c6e08975a49e910a7242efb3
bubbathevtog <bubbathevtog@gmail.com>
2018-04-17 00:56:08 -0400

    Changed the homepage image to be much more mobile friendly.

--- index.php
+++ index.php
@@ -8,7 +8,9 @@
 	<h2>Our Goal</h2>
 	<hr>
 	<picture class="titlePicture">
-		<img src="https://i.imgur.com/N28sxNA.jpg" alt="Bubba's workstation.">
+		<source media="(max-width: 450px)" srcset="https://i.imgur.com/ZyVnVox.png">
+		<source media="(max-width: 1024px)" srcset="https://i.imgur.com/catFsFh.png">
+		<img src="https://i.imgur.com/NsRGs2Q.png" alt="Bubba's workstation.">
 	</picture>
 	<p>
 		POP &amp; LOCK the STACK is a website with references and tutorials on UNIX development for beginners and novice users.
