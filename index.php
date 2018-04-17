249491cee928a1651aa0190593e54d412d435b0a
Bubba Lichvar <bubbathevtog@gmail.com>
2018-04-16 22:44:58 -0400

    fixed up the home page

--- index.php
+++ index.php
@@ -5,18 +5,25 @@
 	include($path.'assets/inc/head.php');
 	// include($path.'assets/inc/nav.php');
 ?>
-	<h1>Home Page</h1><br>
-		
-	<h2>Our goal</h2>
-		
-	<p>POP & LOCK the STACK is a website with references and tutorials on UNIX development for beginners and novice users. 
-	The main goal of this site is to be educative and be a helpful tool for developers to use it as a guide for their work. 
-	This website gives the user an idea of all the basic commands a developer should know to work in an UNIX environment. 
-	It teaches them all the basic commands and flags associated with each command.</p>
-		
-	<h2>A little about us</h2>
-	<p>This site is a free resource for developers and always will be. 
-	POP & LOCK the STACK was created by a group of 5 members as a group project for their web development class.</p>
+	<h2>Our Goal</h2>
+	<picture class="titlePicture">
+		<img src="https://i.imgur.com/N28sxNA.jpg" alt="Bubba's workstation.">
+	</picture>
+	<hr>
+
+	<p>
+		POP &amp; LOCK the STACK is a website with references and tutorials on UNIX development for beginners and novice users.
+		 The main goal of this site is to be educative and be a helpful tool for developers to use it as a guide for their work.
+		 This website gives the user an idea of all the basic commands a developer should know to work in an UNIX environment.
+		 It teaches them all the basic commands and flags associated with each command.
+	</p>
+
+	<h2>A Little About US</h2>
+	<hr>
+	<p>
+		This site is a free resource for developers and always will be.
+		 POP &amp; LOCK the STACK was created by a group of 6 members as a group project for their web development class.
+	</p>
 
 	<?php include($path.'assets/inc/howto.php'); ?>
 
