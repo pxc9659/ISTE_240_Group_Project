4b155421cafec0ee44c27db61dea85a0d3edaff4
Justin Stein <justein230@gmail.com>
2018-04-19 16:44:36 -0400

    Added footer to pages that were missing it, custom page names array.

--- assets/inc/functions.php
+++ assets/inc/functions.php
@@ -1,10 +1,24 @@
 <?php
 
 	function getPageName($pageNamePassed) {
+
+		$page_names = array(
+
+			'user_del_add' => 'useradd/userdel',
+			'group_del_add' => 'groupadd/groupdel'
+
+		);
+
 		$pageNameEdit = explode('_', $pageNamePassed);
 		// Check if introduction is in the array, because if it is formatting isn't really necessary.
 		if (in_array('introduction', $pageNameEdit) || in_array('Introduction', $pageNameEdit)) {
 			$pageNameString = 'Introduction';
+		}
+
+
+		// Check if custom page name is defined
+		else if (array_key_exists($pageNamePassed, $page_names)) {
+			$pageNameString = $page_names[$pageNamePassed];
 		}
 
 		// Occurs if introduction is not in the array
@@ -12,7 +26,13 @@
 			// Add each of the words from the split array to the title string.
 			$pageNameString = '';
 			for ($i = 0; $i < count($pageNameEdit); $i++) {
-				$pageNameString = $pageNameString.$pageNameEdit[intval($i)].' ';
+				if (count($pageNameEdit) <= 1) {
+					$pageNameString = $pageNameString.$pageNameEdit[intval($i)];
+				}
+				else {
+					$pageNameString = $pageNameString.ucfirst($pageNameEdit[intval($i)]).' ';
+				}
+				
 			}
 		}
 
--- pages/permissions/group_del_add.php
+++ pages/permissions/group_del_add.php
@@ -27,3 +27,7 @@
 
 
 ?>
+
+<?php
+	include($path.'assets/inc/footer.php');
+?>
