98eaecde635d2c66518b4afb9cf69df218a42b5b
Justin Stein <justein230@gmail.com>
2018-04-19 22:04:30 -0400

    Removed whitespace from content pages.

--- pages/TheCommandLine/cd.php
+++ pages/TheCommandLine/cd.php
@@ -1,60 +1,27 @@
+<!-- Head PHP Block -->
 <?php
-
+	// Get the page name from the file name
 	$pageName = basename(__FILE__, '.php');
-	
 	$path='../../';
 	$nav=$pageName;
-
-	require($path.'assets/inc/functions.php');
-
+	require($path.'assets/inc/functions.php'); // The file that contains global functions
 	$title='PALTS : '.getPageName($pageName);
 	require($path.'assets/inc/head.php');
-	include($path.'../../groupconn.php');
+	require($path.'../../groupconn.php');
 ?>
-
+<!-- Content PHP Block -->
 <?php
-
+	// Get page data
 	$result = $conn->query('SELECT * FROM Pages WHERE PageName = \''.$pageName.'\'');
-
+	// Read in page data and echo to the webpage
 	while ($resultArr = $result->fetch_assoc()) {
-
 		$pageNameString = getPageName($pageName);
-
 		echo '<hr><h2>'.$resultArr['PageParent'].': '.$pageNameString.'</h2><hr>';
-
 		echo $resultArr['Content'];
 	}
 
 ?>
-
-
-<!-- <picture class="titlePicture">
-	<source media="(max-width: 450px)" srcset="https://i.imgur.com/0KFH9LL.png">
-	<source media="(max-width: 1024px)" srcset="https://i.imgur.com/E2uwS8H.png">
-	<img src="https://i.imgur.com/d9VROEQ.png" alt="An example of the cd command.">
-</picture>
-<p>
-	This is a very simple command, and does not require much explanation.
-	 It stands for “change directory”, and is used to move about the file system in bash.
-	 It does not have any arguments, and has no man page, since it has a very simple purpose.
-	 The structure of the command is ‘cd /folder/to/navigate/to/’.
-	 Note that this command only accepts folder paths, as you can only change into directories.
-	 There are certain ways to shorten and abbreviate paths, described below:
-</p>
-<pre class="preBox">user@hostname$ cd ~</pre>
-<p class="preText">
-	Refers to the users home folder.
-	 This means that the command ‘cd ~/folder’ brings the user to the folder named ‘folder’ in their home directory.
-</p>
-<pre class="preBox">user@hostname$ cd ..</pre>
-<p class="preText">
-	Refers to the parent folder of the current location.
-	 This can be used to refer to files outside of the folder you are in.
-	</p>
-<p>
-	Note that it is also possible to navigate without using ‘absolute’, or full, path names.
-	 If you are located in your home directory, and type ‘cd folder’, it will intelligently deduce that you want to navigate to the folder below where you currently are.
-</p> -->
+<!-- Footer PHP Block -->
 <?php
 	include($path.'assets/inc/footer.php');
 ?>
