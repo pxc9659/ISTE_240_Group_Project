ea013a4e550f190001cf651fb3616705552e1fc5
Justin Stein <justein230@gmail.com>
2018-04-19 22:05:54 -0400

    Removed whitespace from functions.php.

--- assets/inc/functions.php
+++ assets/inc/functions.php
@@ -1,20 +1,19 @@
 <?php
 
 	function getPageName($pageNamePassed) {
-
+		// Define custom page names
 		$page_names = array(
 
 			'user_del_add' => 'useradd/userdel',
 			'group_del_add' => 'groupadd/groupdel'
 
 		);
-
+		// Separate on _, our space replacement
 		$pageNameEdit = explode('_', $pageNamePassed);
 		// Check if introduction is in the array, because if it is formatting isn't really necessary.
 		if (in_array('introduction', $pageNameEdit) || in_array('Introduction', $pageNameEdit)) {
 			$pageNameString = 'Introduction';
 		}
-
 
 		// Check if custom page name is defined
 		else if (array_key_exists($pageNamePassed, $page_names)) {
@@ -31,13 +30,9 @@
 				}
 				else {
 					$pageNameString = $pageNameString.ucfirst($pageNameEdit[intval($i)]).' ';
-				}
-				
+				}				
 			}
 		}
-
 		return $pageNameString;
-
 	} // END getPageName()
-
 ?>
