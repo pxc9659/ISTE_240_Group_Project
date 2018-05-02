fee13735cf5628fdb9a6558cad18605e87811b78
Peter Chesarek (RIT Student) <pxc9659@ist-serenity.main.ad.rit.edu>
2018-05-02 15:47:45 -0400

    I created a new file in the includes that is the into for how to open the terminal. It needs to be added to the command line intro.

---
+++ assets/inc/terminal.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "<h1>How To Get To The Terminal</h1>
+<p>The first step to using any Linux system is to open up the terminal. The Terminal us used for many different processes and there are many useful commands in this guide that you will learn about to aid you. On Linux distributions and MAC OS you can simply search for terminal using the search function and it will open. You can also use Ctrl+Alt+T to open terminal if you are using Ubuntu or Mint, or right-click and choose 'Open in terminal'.</p>
+
+
+
+
+"
+?>
